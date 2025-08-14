a6e9df06b39768277934b69b49cd91cbe00198e5
infraestructura-switchs <infraestructura-switchs@hotmail.com>
2025-08-14 16:28:57 -0500

    Feature/new fatures (#14)
    * fix docker file
    
    * fix docker file
    
    ---------
    
    Co-authored-by: C0576P10N105\LENOVO <spidermejo7@gmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider {
 
@@ -11,11 +12,16 @@ class AppServiceProvider extends ServiceProvider {
 
     }
 
-
-    public function boot() {
+    public function boot(UrlGenerator $url)
+    {
         date_default_timezone_set('America/Bogota');
-        Blade::directive('formatToCop', function ($value) {
-            return "<?php echo '$ ' . number_format($value, 0, '.', ','); ?>";
-        });
+                Blade::directive('formatToCop', function ($value) {
+                    return "<?php echo '$ ' . number_format($value, 0, '.', ','); ?>";
+                });
+
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
     }
+
 }
